b8e4cc033d610780a8b1929cec42d5a681a4d4bb
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-01-09 17:01:43 +0600

    tailwind config added on project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          "color-primary": "#707070",
+          "golden-primary": "#B68C5A",
+          "color-title":"#414040",
+        }
+      },
+    },
+    plugins: [],
+  }
